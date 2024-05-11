5dc88a056c11d9ba325e7416230e0509b0842401
Katarina Pejcinovic <67714883+Katarina-Pejcinovic@users.noreply.github.com>
2024-05-10 23:07:29 -0700

    CV runs and prints images

--- U_Net.py
+++ U_Net.py
@@ -1,7 +1,7 @@
 import os
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from keras import layers, models
 import numpy as np
 import pickle 
 from make_sample_test import make_smaller
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from PIL import Image
 from sklearn.model_selection import StratifiedKFold
+from pandas.plotting import table 
+
 # from tensorflow.keras.utils import plot_model
 # print("Available devices:")
 # print(tf.config.list_physical_devices())
@@ -32,7 +34,7 @@ class BatchLossHistory(tf.keras.callbacks.Callback):
         #plt.show()
         plt.savefig('figures/batch_loss.png')
 
-def weighted_BCE(target, output, weights = [200, 1]):
+def weighted_BCE(target, output, weights = [1, 200]):
     target = tf.convert_to_tensor(target)
     output = tf.convert_to_tensor(output)
     weights = tf.convert_to_tensor(weights, dtype=target.dtype)
@@ -48,7 +50,6 @@ def weighted_BCE(target, output, weights = [200, 1]):
 ######################### define U-net architecture ################################
 
 def double_conv_block(x, n_filters):
-
    # Conv2D then ReLU activation
    x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
    # Conv2D then ReLU activation
@@ -75,7 +76,7 @@ def upsample_block(x, conv_features, n_filters):
 
    return x
 
-def build_unet_model():
+def build_unet_base():
  # inputs
    inputs = layers.Input(shape=(512, 512, 1))
 
@@ -102,13 +103,31 @@ def build_unet_model():
    # 9 - upsample
    u9 = upsample_block(u8, f1, 32)
 
-   # outputs
-   outputs = layers.Conv2D(1, (1,1), padding="same", activation = 'sigmoid')(u9) 
+   return inputs, u9
 
-   # unet model with Keras Functional API
-   unet_model = tf.keras.Model(inputs, outputs, name="U-Net")
+def threshold_lambda(x, threshold=0.5):
+    thresholded = tf.cast(x > threshold, tf.float32)
+    tf.print("Thresholded Output:", thresholded)
+    return thresholded
 
-   return unet_model
+def print_tensor(x, message= "output"):
+    tf.print(message, x)
+    return x 
+
+def build_unet_training_model():
+    inputs, last_layer_output = build_unet_base()
+    outputs = layers.Conv2D(1, (1, 1), padding="same", activation='sigmoid')(last_layer_output)
+    printed_outputs = layers.Lambda(print_tensor, arguments={'message': "Conv2D Output:"})(outputs)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs, name="U-Net-Training")
+    return model
+
+#inference model contains thresholding
+def build_unet_inference_model(threshold):
+    inputs, last_layer_output = build_unet_base()
+    outputs = layers.Conv2D(1, (1, 1), padding="same", activation='sigmoid')(last_layer_output)
+    thresholded_output = layers.Lambda(threshold_lambda, arguments={'threshold': threshold})(outputs)
+    model = tf.keras.Model(inputs=inputs, outputs=thresholded_output, name="U-Net-Inference")
+    return model
 
 def dice_coefficient(y_true, y_pred, smooth=1e-6):
     
@@ -126,11 +145,8 @@ def dice_coefficient(y_true, y_pred, smooth=1e-6):
     dice = (2. * intersection + smooth) / (union + smooth)
     return dice
 
-unet_model = build_unet_model()
-# plot_model(unet_model, to_file='unet_model.png', show_shapes=True, show_layer_names=True)
-unet_model.compile(optimizer=tf.keras.optimizers.Adam(),
-                  loss= 'binary_crossentropy',
-                  metrics= [dice_coefficient])
+
+# # plot_model(unet_model, to_file='unet_model.png', show_shapes=True, show_layer_names=True)
 
 #make dataset tensorflow-compatible
 with open('data/images_data.pkl', 'rb') as f:
@@ -147,7 +163,7 @@ with open('data/masks_data.pkl', 'rb') as f:
 
 # Batches
 batch_size = 30
-epochs = 1
+epochs = 30
 #dataset = dataset.batch(batch_size).repeat(count = epochs)
 total_num_samples = images.shape[0]
 steps = total_num_samples//batch_size
@@ -216,17 +232,29 @@ def predict_in_batches(data, numbers, batch_size=30):
         print("predictions thresh", predictions_thresh.shape)
         #save_images_as_png(predictions_thresh, batch_numbers, 'output_images')
 
-def display_image_and_mask(image, mask):
+def display_image_and_mask(image, mask, predicted, thresholded):
 
     plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 4, 1)
     plt.imshow(image[:, :, 0], cmap='gray')  # Display the first channel (grayscale)
     plt.title('Original Image')
     plt.axis('off')
+    
 
-    plt.subplot(1, 2, 2)
-    plt.imshow(mask[:, :, 0], cmap='gray')  # Display the first channel of mask
+    plt.subplot(1, 4, 2)
+    plt.imshow(mask[:, :, 0], cmap='gray')  
+    plt.title('OG mask')
+    plt.axis('off')
+
+    plt.subplot(1, 4, 3)
+    plt.imshow(predicted[:, :, 0], cmap='gray')  
     plt.title('Predicted Mask')
+    plt.axis('off')
+    print("image printed", predicted[:, :, 0])
+
+    plt.subplot(1, 4, 4)
+    plt.imshow(thresholded[:, :, 0], cmap='gray')  
+    plt.title('Thresholded Mask')
     plt.axis('off')
 
     plt.show()
@@ -235,10 +263,13 @@ def display_image_and_mask(image, mask):
 with open('data/strat_labels.pkl', 'rb') as f:
     labels = pickle.load(f)
 
-#numbers for saving images 
+#numbers for saving image IDs 
 with open('data/test_numbers.pkl', 'rb') as f:
     numbers = pickle.load(f)
 print("numbers", numbers.shape)
+
+# images = make_smaller(images)
+# masks = make_smaller(masks)
 
 # Number of splits
 n_splits = 3
@@ -246,43 +277,72 @@ n_splits = 3
 # Create the StratifiedKFold object
 skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
 print("starting cross validation")
-metrics_summary = pd.DataFrame(columns=['Fold', 'Dice Coefficient'])
+metrics_summary = pd.DataFrame(columns=['Fold', 'Loss', 'Dice Coefficient', 'Recall'])
 
 fold_no = 1
 for train_index, val_index in skf.split(images, labels):
     train_dataset = tf.data.Dataset.from_tensor_slices((images[train_index], masks[train_index]))
     train_dataset = train_dataset.batch(batch_size).repeat(count = epochs)
     val_dataset = tf.data.Dataset.from_tensor_slices((images[val_index], masks[val_index]))
-    val_dataset = val_dataset.batch(batch_size)  # Assuming batch_size is defined
+    val_dataset = val_dataset.batch(batch_size) 
+    # Extract only the images using a lambda function
+    image_dataset = val_dataset.map(lambda image, _: image)
 
     # for images, masks in dataset.take(1):
-    #     print("Batched images shape:", images.shape)  # e.g., (16, 512, 512, 1)
-    #     print("Batched masks shape:", masks.shape)    # e.g., (16, 512, 512, 1)
+    #     print("Batched images shape:", images.shape)  
+    #     print("Batched masks shape:", masks.shape)   
     
     # train model
     print(f"Training on fold {fold_no}")
-    unet_model = build_unet_model()
+    unet_model = build_unet_training_model()
     unet_model.compile(optimizer=tf.keras.optimizers.Adam(),
-                  loss= 'binary_crossentropy',
-                  metrics= [dice_coefficient])
+                  loss= weighted_BCE,
+                  metrics= [dice_coefficient, tf.keras.metrics.Recall()])
+    
     steps = images[train_index].shape[0]//batch_size
-    print("steps in fold", steps)
     model_history = unet_model.fit(train_dataset, epochs = epochs, steps_per_epoch = steps, callbacks=[batch_loss_history])
     
     #validate model
     print("validating model")
+
     #predict_in_batches(val_images, numbers)
-    eval_metrics = unet_model.evaluate(val_dataset, verbose=0)
+    unet_inference_model = build_unet_inference_model(threshold=0.5)
+    unet_inference_model.set_weights(unet_model.get_weights())
+    unet_inference_model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss= 'binary_crossentropy',
+                  metrics= [dice_coefficient, tf.keras.metrics.Recall()])
+    
+    eval_metrics = unet_inference_model.evaluate(val_dataset, verbose=0)
+ 
+    masks_pred = unet_model.predict(image_dataset)
+    print("masks pred", masks_pred[:, :, 0])
+    masks_thresh = unet_inference_model.predict(image_dataset)
+
+    #print images of original, mask, and predicted 
+    # for i in range(images[val_index].shape[0]):
+    #     display_image_and_mask(images[val_index][i], masks[val_index][i], masks_pred[i], masks_thresh[i])
+
     # Create a DataFrame for the current fold's metrics
     current_fold_metrics = pd.DataFrame({
         'Fold': [fold_no],
         'Loss': [eval_metrics[0]],
-        'Dice Coefficient': [eval_metrics[1]]
+        'Dice Coefficient': [eval_metrics[1]],
+        'Recall': [eval_metrics[2]]
     })
 
     # Concatenate the current fold's metrics DataFrame with the main summary DataFrame
     metrics_summary = pd.concat([metrics_summary, current_fold_metrics], 
                                 ignore_index=True)
+    
+    # Plotting the DataFrame as a table
+    fig, ax = plt.subplots(figsize=(8, 3))  # Set appropriate size for your table
+    ax.axis('tight')
+    ax.axis('off')
+    the_table = table(ax, metrics_summary, loc='center')
+
+    # Save the figure as a PNG file
+    plt.savefig('figures/dataframe_table.png', dpi=300)  # Adjust dpi for different resolutions
+    plt.close(fig)  # Close the figure
 
     fold_no += 1
 
